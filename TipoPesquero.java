eb374a87c20d1b6c78001cbea9eb2946b7414fb7
Jaymar Neshawn Warren Mcglocklin <Jaymarbrooks101@gmail.com>
2024-08-12 16:29:14 -0600

    Commit 2 - Creacion de Enum Tipo Pesquero

---
+++ TipoPesquero.java
@@ -0,0 +1,15 @@
+public enum TipoPesquero {
+    PEZ(10.0),
+    CAMARON(15.0),
+    LANGOSTA(20.0);
+
+    public final double price;
+
+    TipoPesquero(double price) {
+        this.price = price;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+}
