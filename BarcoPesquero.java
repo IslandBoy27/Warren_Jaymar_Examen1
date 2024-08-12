836e1938329b6d0299b0aeed82e25c867f5a98e3
Jaymar Neshawn Warren Mcglocklin <Jaymarbrooks101@gmail.com>
2024-08-12 16:47:10 -0600

    Commit 4 - Creacion Clase Hija BarcoPesquero

---
+++ BarcoPesquero.java
@@ -0,0 +1,32 @@
+public final class BarcoPesquero extends Barco {
+    private int pecesCapturados;
+    private final TipoPesquero tipoPesquero;
+
+    public BarcoPesquero(String nombre, TipoPesquero tipoPesquero) {
+        super(nombre);
+        this.pecesCapturados = 0;
+        this.tipoPesquero = tipoPesquero;
+    }
+
+    @Override
+    public void agregarElemento() {
+        pecesCapturados += 1;
+    }
+
+    @Override
+    public double vaciarCobrar() {
+        double total = pecesCapturados * tipoPesquero.price;
+        pecesCapturados = 0;
+        return total;
+    }
+
+    @Override
+    public double precioElemento() {
+        return tipoPesquero.price;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + " (Tipo: " + tipoPesquero + ", Peces Capturados: " + pecesCapturados + ")";
+    }
+}
